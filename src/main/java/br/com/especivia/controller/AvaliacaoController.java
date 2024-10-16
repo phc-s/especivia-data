c92edce18e44892fb77ade2a8ce3251286d7b778
PHC-S <pedrohenriquecs18@gmail.com>
2024-10-16 17:49:59 -0300

    Added controllers.

---
+++ src/main/java/br/com/especivia/controller/AvaliacaoController.java
@@ -0,0 +1,31 @@
+package br.com.especivia.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AvaliacaoController {
+
+    @GetMapping("/get")
+    public String get() {
+        return "Request GET";
+    }
+
+    @PostMapping("/post")
+    public String post() {
+        return "Request POST";
+    }
+
+    @PutMapping("/put")
+    public String put() {
+        return "Request PUT";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete() {
+        return "Request DELETE";
+    }
+
+}
